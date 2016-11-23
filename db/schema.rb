adf190e1936b13e18785bb053f9c583c753999bf
hayato-shirasaka <hayato@hayato-no-MacBook-Pro.local>
2016-11-23 12:39:30 +0900

    remove model

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20161122132919) do
 
-  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
