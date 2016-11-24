482b11ee0068c228417d9953230b49c991f1016d
hayato-shirasaka <hayato@hayato-no-MacBook-Pro.local>
2016-11-24 20:11:14 +0900

    from review readjustment route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :messages
-  resources :groups
+  resources :groups, only: [:new, :create] do
+    resources :messages, only: :index
+  end  
 end
