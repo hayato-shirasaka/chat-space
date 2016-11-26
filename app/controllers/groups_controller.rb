a0a0500229e98587e0a25eb7e9916ffa10d77f85
hayato-shirasaka <hayato@hayato-no-MacBook-Pro.local>
2016-11-26 19:39:33 +0900

    add actions to controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,6 +13,17 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end    
+
   private
   def group_params
     params.require(:group).permit(:name, user_id: [])
