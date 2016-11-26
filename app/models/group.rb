91221de53f172634a053dcd92fb01045a4299e91
hayato-shirasaka <hayato@hayato-no-MacBook-Pro.local>
2016-11-26 16:53:51 +0900

    application accepts_nested_attributes_for

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,7 @@
 class Group < ApplicationRecord
+  has_many :messages
+  has_many :users, through: :groups_users
+  has_many :group_users
+  accepts_nested_attributes_for :group_users
   validates :name, presence: true
 end
