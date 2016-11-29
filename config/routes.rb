06b85504ef0d57e1974c0183e805a92402bf7681
hayato-shirasaka <hayato@hayato-no-MacBook-Pro.local>
2016-11-29 12:15:34 +0900

    add routing message

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: :index
+    resources :messages, only: [:index, :create]
   end
 end
