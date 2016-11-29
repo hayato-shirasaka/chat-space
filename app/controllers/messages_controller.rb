48c6ff272d33de21fb79a56379c59dc8da38168a
hayato-shirasaka <hayato@hayato-no-MacBook-Pro.local>
2016-11-29 22:43:59 +0900

    change index and path

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,21 +2,22 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @messages = Message.all
+    @group = Group.find(params[:group_id])
     @message = Message.new
   end
 
   def create
     @message =Message.new(message_params)
     if @message.save
-      redirect_to root_path, notice: "メッセージを投稿しました"
+      redirect_to group_messages_path, notice: "メッセージを投稿しました"
     else
       flash.now[:alert] = "メッセージが投稿できません"
       render action: :index
     end
   end
 
+  private
   def message_params
-    params.require(:message).permit(:body).merge(user_id: current_user.id)
+    params.require(:message).permit(:body).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
