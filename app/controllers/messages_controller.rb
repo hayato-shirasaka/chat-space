ed6f0f472518d780ecb899174bc4c524c5810dc3
hayato-shirasaka <hayato@hayato-no-MacBook-Pro.local>
2016-11-29 17:31:03 +0900

    routing message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,22 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
-  def index
 
-  end  
+  def index
+    @messages = Message.all
+    @message = Message.new
+  end
+
+  def create
+    @message =Message.new(message_params)
+    if @message.save
+      redirect_to root_path, notice: "メッセージを投稿しました"
+    else
+      flash.now[:alert] = "メッセージが投稿できません"
+      render action: :index
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:body).merge(user_id: current_user.id)
+  end
 end
