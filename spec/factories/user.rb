5acf47f77736ff2b8dc07b3fb27ac6e01b930e4a
hayato-shirasaka <hayato@hayato-no-MacBook-Pro.local>
2016-12-03 13:16:46 +0900

    modify factorygirl

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
 
   factory :user do
-    name                       "chat-space"
-    email                      ""
+    name                       {Faker::Name.name}
+    email                      {Faker::Internet.email}
     password                   "12345678"
     password_confirmation      "12345678"
   end
