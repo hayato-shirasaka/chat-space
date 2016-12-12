3753cd4aed41c4f8e2c5e2461017e4aa2ebd946d
hayato-shirasaka <hayato@hayato-no-MacBook-Pro.local>
2016-12-12 17:47:02 +0900

    modify users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where('name LIKE ?', "%#{params[:name]}%")
-
-    respond_to do |format|
-      format.json {render json: @users}
-    end
+    render json: @users
   end
 end
